e96c1a2165f614307b4f4aaec1fba40643ba09d6
KimCookieYa <min49590@gmail.com>
2024-02-11 00:33:29 +0900

    design: tailwind px setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: (() => {
+        let widths = {};
+        for (let i = 1; i <= 1000; i++) {
+          widths[i] = `${i}px`;
+        }
+        return widths;
+      })(),
+      height: (() => {
+        let height = {};
+        for (let i = 1; i <= 1000; i++) {
+          height[i] = `${i}px`;
+        }
+        return height;
+      })(),
+      padding: (() => {
+        let padding = {};
+        for (let i = 1; i <= 1000; i++) {
+          padding[i] = `${i}px`;
+        }
+        return padding;
+      })(),
+      margin: (() => {
+        let margin = {};
+        for (let i = 1; i <= 1000; i++) {
+          margin[i] = `${i}px`;
+        }
+        return margin;
+      })(),
+    },
   },
   plugins: [],
 };
