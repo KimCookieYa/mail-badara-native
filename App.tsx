dfc613d786e6cfb6cb353ceea537f1cbe768e797
KimCookieYa <min49590@gmail.com>
2024-02-11 00:33:47 +0900

    feat: webview setting

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Dimensions, View } from "react-native";
+import WebView from "react-native-webview";
+
+const DEVICE_HEIGHT = Dimensions.get("window").height;
+const DEVICE_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text>Hello, World!</Text>
+    <View className="flex-1">
       <StatusBar style="auto" />
+      <WebView
+        source={{ uri: "https://google.com" }}
+        style={{ width: DEVICE_WIDTH, height: DEVICE_HEIGHT }}
+      />
     </View>
   );
 }
